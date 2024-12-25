cc1abde7180441f70af36867aeda06ee865f0fe8
Rafael Angonese <rafael.angonese09@gmail.com>
2024-12-25 19:56:09 -0300

    :bug: Fix(login): remove normalize email

--- app/validators/auth/login_validator.ts
+++ app/validators/auth/login_validator.ts
@@ -2,7 +2,7 @@ import vine from '@vinejs/vine'
 
 export const loginValidator = vine.compile(
   vine.object({
-    email: vine.string().email().normalizeEmail(),
+    email: vine.string().email(),
     password: vine.string(),
   })
 )
